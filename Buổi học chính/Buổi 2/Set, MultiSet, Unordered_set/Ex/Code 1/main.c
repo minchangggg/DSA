a211bb355d6306ccf6c253d722fd1b320df13923
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-06-20 15:10:01 +0700

    Create main.c

---
+++ Buổi học chính/Buổi 2/Set, MultiSet, Unordered_set/Ex/Code 1/main.c
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    #ifndef HAHA
+    freopen ("input.txt", "r", stdin);
+    freopen ("output.txt", "w", stdout);
+    #endif
+
+    int n; cin >> n; 
+    int a[n];
+    for (int &x : a) cin >> x;
+    
+    set <int> se;
+    for (int x: a) {
+        if (se.find(x) != se.end()) cout << x << ' ';
+        se.insert(x);
+    }
+}
