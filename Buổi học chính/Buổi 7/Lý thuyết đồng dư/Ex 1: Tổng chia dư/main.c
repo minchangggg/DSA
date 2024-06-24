55867a84c812639b48953d2d780b1d151210d6d2
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2024-06-24 23:41:39 +0700

    Create main.c

---
+++ Buổi học chính/Buổi 7/Lý thuyết đồng dư/Ex 1: Tổng chia dư/main.c
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+const ll mod = 1e9+7;
+
+int main() {
+    freopen("input.TXT", "r", stdin);
+    freopen("output.TXT", "w", stdout);
+
+    int n = 0; cin >> n;
+    ll s = 0;
+
+    for(ll i = 0; i < n; i++) {
+        ll temp; cin >> temp;
+        s += temp;
+        s %= mod;
+    }
+    cout << s;
+}
